3961006ccf118094f41011a0af959d113eb2c2e4
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-05-09 13:12:28 -0300

    Melhorando o teste para o Expense Report Controller

--- spec/features/user_create_report_add_expense_spec.rb
+++ spec/features/user_create_report_add_expense_spec.rb
@@ -4,11 +4,6 @@ feature 'User create a report and add to it a expense' do
   scenario 'successfully' do
     # Criação dos dados
     user = User.create!(email: 'glauco.margarido@gmail.com', password: '12345678')
-    expense_report = ExpenseReport.create!(title: 'Despesas de Abril',
-                                          start_date: '01/04/2018',
-                                          end_date: '30/04/2018',
-                                          own_car: false,
-                                          user: user)
 
     expense_type = ExpenseType.create!(description: 'Despesa com Hospedagem')
 
@@ -21,7 +16,12 @@ feature 'User create a report and add to it a expense' do
     fill_in 'Email', with: 'glauco.margarido@gmail.com'
     fill_in 'Senha', with: '12345678'
     click_on 'Log in'
-    click_on 'Despesas de Abril'
+    click_on 'Cadastrar um relatório de despesas'
+    fill_in 'Descrição', with: 'Despesas de Abril'
+    fill_in 'Data inicial', with: '01/04/2018'
+    fill_in 'Data final', with: '30/04/2018'
+    choose 'expense_report_own_car_false'
+    click_on 'Cadastrar'
     click_on 'Cadastrar uma despesa'
     select 'Almoço', from: 'Classificação'
     fill_in 'Valor', with: '25.9'
@@ -31,5 +31,31 @@ feature 'User create a report and add to it a expense' do
     expect(page).to have_css('h3', text: 'Despesas de Abril')
     expect(page).to have_css('h6', text: 'Almoço')
     expect(page).to have_css('li', text: '25.9')
+  end
+
+  scenario 'and must all expense report fields' do
+    # Criação dos dados
+    user = User.create!(email: 'glauco.margarido@gmail.com', password: '12345678')
+
+    expense_type = ExpenseType.create!(description: 'Despesa com Hospedagem')
+
+    expense_subtype = ExpenseSubtype.create!(description: 'Almoço',
+                                    accounting_account: '632.250',
+                                    expense_type_id: expense_type.id)
+
+    # Navegação
+    visit root_path
+    fill_in 'Email', with: 'glauco.margarido@gmail.com'
+    fill_in 'Senha', with: '12345678'
+    click_on 'Log in'
+    click_on 'Cadastrar um relatório de despesas'
+    fill_in 'Descrição', with: ''
+    fill_in 'Data inicial', with: '01/04/2018'
+    fill_in 'Data final', with: '30/04/2018'
+    choose 'expense_report_own_car_false'
+    click_on 'Cadastrar'
+
+    # Expectativa
+    expect(page).to have_content('Não foi possível criar este relatório.')
   end
 end
